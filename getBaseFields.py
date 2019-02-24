d4f6ad22fa4344c8f40ce264d5081ddb80cefc8f
Andrew <stajc06@moravian.edu>
2019-02-24 13:51:35 -0500

    Added read/write functions for use with .csv file to interface, along with tests

--- getBaseFields.py
+++ getBaseFields.py
@@ -1,37 +1,73 @@
 import xml.etree.ElementTree as ET
+import csv
 
 
 class xmlFieldInterpreter():
 
     def __init__(self, xmlFile):
-        # self.setRoot(xmlFile)
         self.tree = ET.parse(xmlFile)
         self.root = self.tree.getroot()
-        # self.tree = ET.parse(self.CurrentxmlFileName)
-        # self.root = self.tree.getroot()
-
-    # def setRoot(self, xmlFileName):
-    #     self.tree = ET.parse(xmlFileName)
-    #     self.root = self.tree.getroot()
-    #
-    # def testPrint(self):
-    #     for child in self.root:
-    #         print(child.tag, child.text)
 
     def getNCTid(self):
         for child in self.root:
             if child.tag == "id_info":
                 for grand_child in child:
                     if grand_child.tag == "nct_id":
-                        return [grand_child.text, grand_child.tag]
+                        return [grand_child.tag, grand_child.text]
 
-    def getTopLevelFields(self):
+    def getTopLevelFieldsTags(self):
         top_fields = []
         for child in self.root:
-            current_child_info = [child.tag, child.text]
-            top_fields.append(current_child_info)
+            top_fields.append(child.tag)
         return top_fields
 
     def getFieldsWithID(self):
-        id_info_pair = [self.getNCTid(), self.getTopLevelFields()]
+        id_info_pair = [self.getNCTid(), self.getTopLevelFieldsTags()]
         return id_info_pair
+
+    def doesElementHaveChildren(self, currentElement):
+        for child in currentElement:
+            return True
+        else:
+            return False
+
+    def getBaseFieldTags(self):
+        elementFieldTags = []
+        elementFieldTags.append(self.getNCTid()[0])
+        for child in self.root:
+            if not self.doesElementHaveChildren(child):
+                elementFieldTags.append(child.tag)
+        return elementFieldTags
+
+    def getBaseFieldValues(self):
+        elementFieldValues = []
+        elementFieldValues.append(self.getNCTid()[1])
+        for child in self.root:
+            if not self.doesElementHaveChildren(child):
+                elementFieldValues.append(child.text)
+        return elementFieldValues
+    
+    def writeFieldNamesWithIDToFile(self):
+        if self.isCSVFileEmpty():
+            with open('clinicalTrialData.csv', mode='w') as trial_data_file:
+                trial_data_writer = csv.writer(trial_data_file, delimiter=',')
+                trial_data_writer.writerow(self.getBaseFieldTags())
+
+    def writeFieldValuesWithIDToFile(self):
+        if not self.isCSVFileEmpty():
+            with open('clinicalTrialData.csv', mode='a') as trial_data_file:
+                trial_data_writer = csv.writer(trial_data_file, delimiter=',')
+                trial_data_writer.writerow(self.getBaseFieldValues())
+
+    def isCSVFileEmpty(self):
+        try:
+            with open("clinicalTrialData.csv", mode='r') as csv_file:
+                csv_reader = csv.reader(csv_file, delimiter=',')
+                linecount = 0
+                for row in csv_reader:
+                    if not row == []:
+                        linecount += 1
+                return linecount == 0
+        except IOError:
+            file = open("clinicalTrialData.csv", mode='w')
+            return True
