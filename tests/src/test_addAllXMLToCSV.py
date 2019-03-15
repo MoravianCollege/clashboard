2be6871005ccd0459c5bd901eba91e6b4028d271
JohnPolich <stjnp20@moravian.edu>
2019-03-15 15:12:42 -0400

    Cleaned up Clashboard repository

--- tests/src/test_addAllXMLToCSV.py
+++
@@ -1,8 +0,0 @@
-from unittest import TestCase
-from xml_to_csv_conversion.src.createDepthOneCSVFile import depthOneCSVWriter
-
-class test_addALLXMLToCSV(TestCase):
-
-    def test_AddAllDepthOneXMLValuesToCSV(self):
-        csv_writer = depthOneCSVWriter
-        depthOneCSVWriter.fill_depth_one_csv(csv_writer)
--- xml_to_csv_conversion/src/xmlElementInformation.py
+++
@@ -1,9 +0,0 @@
-
-
-class xmlElementInformation:
-
-    def __init__(self):
-        self.tag_list = []
-        self.text_list = []
-        self.condition_list = []
-        self.keyword_list = []
