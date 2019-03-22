98e83c0d0674c1ec16b3d759ac4d7a55bddd1d73
Nick Zambelli <stnvz01@moravian.edu>
2019-03-22 14:30:45 -0400

    Add comments explaining what each function should do, parameters it should take, and data structures to return

--- src/clashboard/clash_interface.py
+++ src/clashboard/clash_interface.py
@@ -6,20 +6,38 @@ class ClashInterface:
         pass
 
     # Uncomment when tested and implemented
-    # def remove_filter(self, filter):
-    #    pass
+    def remove_filter(self, filter_category, filter_name):
+        """
+        Removes a specified filter from the list of filters, and re-runs the query
+        :param filter_category: the column in the DB that the user wants to filter
+        :param filter_name: the specific value to filter on in that column
+        """
+        pass
 
     # Uncomment when tested and implemented
-    # def apply_filter(self, filter):
-    #    pass
+    def apply_filter(self, filter_category, filter_name):
+        """
+        Adds the specified filter to the list of filters, and re-runs the current query
+        :param filter_category: the column in the DB that the user wants to filter
+        :param filter_name: the specific value to filter on in that column
+        """
+        pass
 
     # Uncomment when tested and implemented
-    # def get_current_filters(self):
-    #    pass
+    def get_current_filters(self):
+        """
+        Get the list of currently applied filters
+        :return: the list of human-readable strings
+        """
+        pass
 
     # Uncomment when tested and implemented
-    # def set_group_by(self, attribute):
-    #    pass
+    def set_group_by(self, attribute):
+        """
+        Sets the attribute to group the data by, runs the query for the first
+        :param attribute: human-readable string
+        """
+        pass
 
     def get_group_by(self):
         """
@@ -29,13 +47,27 @@ class ClashInterface:
         return ''
 
     # Uncomment when tested and implemented
-    # def get_labels(self):
-    #    pass
+    def get_labels(self):
+        """
+        Get the lists of strings describing each category for grouping the data
+        :return: list of human-readable strings
+        """
+        pass
 
     # Uncomment when tested and implemented
-    # def get_values(self):
-    #    pass
+    def get_values(self):
+        """
+        Get the list of integers describing the amounts for each label
+        :return: an array of ints
+        """
+        pass
 
     # Uncomment when tested and implemented
-    # def get_variables(self):
-    #    pass
+    def get_variables(self):
+        """
+        Get the variables describing the options for displaying data
+        :return: a list of dictionaries containing the label for human-readable display
+        and the corresponding string in terms of the XML Schema
+        ex - {'label': 'Study Type', 'value': 'study_type'}
+        """
+        pass
