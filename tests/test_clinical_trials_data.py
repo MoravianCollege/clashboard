3176dfb40ef646f0843d70090873830ef34c934a
Ben Coleman <colemanb@moravian.edu>
2019-03-23 13:04:39 -0400

    Change tests to mock out populate_tables.
    This version uses a monkey patch to replace the populate_tables
    method in ClinicalTrailsData.
    
    In addition, the tests are re-written to emphasize behavior.

--- tests/test_clinical_trials_data.py
+++ tests/test_clinical_trials_data.py
@@ -1,30 +1,42 @@
 
-from tests.mocks.mock_trials_data import MockClinicalTrialsData
+from clashboard.clinical_trials_data import ClinicalTrialsData
+from pathlib import Path
+import pandas as pd
+
+TEST_DATA_DIR = Path(__file__).resolve().parent / 'data'
 
 
-def test_new_instance_has_no_group():
-
-    mctd = MockClinicalTrialsData()
-    assert mctd.get_group_by() == ''
-
-
-def test_change_group_by():
-
-    mctd = MockClinicalTrialsData()
-    mctd.populate_tables()
-    assert mctd.get_group_by() == ''
-    mctd.set_group_by('study_type')
-    assert mctd.get_group_by() == 'study_type'
+def mock_populate_tables(self):
+    self.studies = pd.read_csv('{}/trial_test_data.csv'.format(TEST_DATA_DIR))
+    self.curr_group = 'phase'
 
 
-def test_get_some_data():
+def test_new_instance_produces_empty_data():
+    ctd = ClinicalTrialsData()
+    assert ctd.get_group_by() == ''
+    assert ctd.get_current_filters() == []
+    assert ctd.get_labels() == []
+    assert ctd.get_values() == []
 
-    mctd = MockClinicalTrialsData()
-    mctd.populate_tables()
-    mctd.set_group_by('phase')
-    assert mctd.get_labels()[0] == 'Phase 2'
-    assert mctd.get_labels()[1] == 'Phase 3'
-    assert mctd.get_labels()[2] == 'Phase 4'
-    assert mctd.get_values()[0] == 1
-    assert mctd.get_values()[1] == 2
-    assert mctd.get_values()[2] == 3
+
+def test_get_some_data(monkeypatch):
+    monkeypatch.setattr(ClinicalTrialsData, 'populate_tables', mock_populate_tables)
+    ctd = ClinicalTrialsData()
+    ctd.populate_tables()
+    assert ctd.get_group_by() == 'phase'
+    assert ctd.get_values() == [1, 2, 3]
+    assert ctd.get_labels() == ['Phase 2', 'Phase 3', 'Phase 4']
+    assert ctd.get_current_filters() == []
+
+
+def test_change_group_by_changes_data(monkeypatch):
+    monkeypatch.setattr(ClinicalTrialsData, 'populate_tables', mock_populate_tables)
+    ctd = ClinicalTrialsData()
+    ctd.populate_tables()
+    ctd.set_group_by('study_type')
+    assert ctd.get_group_by() == 'study_type'
+    assert ctd.get_values() == [9, 1]
+    assert ctd.get_labels() == ['Interventional', 'Observational [Patient Registry]']
+    assert ctd.get_current_filters() == []
+
+
