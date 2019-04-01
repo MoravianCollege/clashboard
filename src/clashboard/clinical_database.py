4ac5c5f9c8520d5c4c46160d6cfcddc9962a4fa5
Charles-Peeke <stcdp03@moravian.edu>
2019-04-01 15:59:28 -0400

    Initial Database Schema

---
+++ src/clashboard/clinical_database.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+
+def db_query(tables = ['studies'], filters = [], groupby = ''):
+    """
+    New SQL QUERY
+    :param tables:
+    :param filters:
+    :param groupby:
+    :return:
+    """
+    pass
+
+
+def update_data(groupby = ''):
+    """
+    Update GROUP BY
+    :param groupby:
+    :return:
+    """
+    pass
