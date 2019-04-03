7e5f2644cd3917c163841c3baf84488be71376c1
Charles-Peeke <stcdp03@moravian.edu>
2019-04-03 15:51:09 -0400

    Further development for database functions
    Currently under development

--- src/clashboard/clinical_database.py
+++ src/clashboard/clinical_database.py
@@ -1,21 +1,30 @@
 import pandas as pd
 
 
-def db_query(tables = ['studies'], filters = [], groupby = ''):
-    """
-    New SQL QUERY
-    :param tables:
-    :param filters:
-    :param groupby:
-    :return:
-    """
-    pass
+
+class ClinicalTrialsQuery:
+    def __init__(self):
+        sql_command = ''
+        trials_data = pd.DataFrame()
+        is_called = False
 
 
-def update_data(groupby = ''):
-    """
-    Update GROUP BY
-    :param groupby:
-    :return:
-    """
-    pass
+    def db_query(self, filters = [], group = ''):
+        """
+        New SQL QUERY
+        :param filters:
+        :param groupby:
+        :return:
+        """
+        self.sql_command = "SELECT * FROM ctgov.studies"
+        self.is_called = True
+        return self.trials_data
+
+
+    def update_data(self, group = ''):
+        """
+        Update GROUP BY
+        :param groupby:
+        :return:
+        """
+        return self.trials_data.groupby([group]) if self.is_called else None
