3ef012126b086614e2d0bd43eb31ed1c6ba53561
JohnPolich <stjnp20@moravian.edu>
2019-04-05 15:35:54 -0400

    added bar graph support with included tests

--- src/clashboard/clashboard_gui.py
+++ src/clashboard/clashboard_gui.py
@@ -13,77 +13,97 @@ phase_counts = None
 currentGroupBy = None
 count = 0
 
+Date = '04/05/2019'
+
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
+
 app.layout = html.Div(children=[
-   html.H1(children='ClinicalTrails.gov Data Exploration'),
+    html.H1(children='ClinicalTrails.gov Data Exploration'),
 
-   html.Div(
-       dash_table.DataTable(
-           id='adding-rows-table',
-           columns=[{
-               'name': 'Filters',
-               'id': 'column-{}'.format(i),
-               'deletable': False,
-               'editable_name': False
-           } for i in range(1)],
-           data=[
-               {'Column-{}'.format(i): (j + (i - 1) * 1) for i in range(1)}
-               for j in range(0)
-           ],
-           row_deletable=False,
-           row_selectable='multi',
-       ),
+    dcc.Dropdown(
+        options=[
+            {'label': 'Bar Graph', 'value': 'bar_chart'},
+            {'label': 'Pie Chart', 'value': 'pie_chart'}
+        ],
+        value='pie_chart',
+        id='chart-type',
+        style={'width': '200'}
+    ),
+    html.Div(
+        dash_table.DataTable(
+            id='adding-rows-table',
+            columns=[{
+                'name': 'Filters',
+                'id': 'column-{}'.format(i),
+                'deletable': False,
+                'editable_name': False
+            } for i in range(1)],
+            data=[
+                {'Column-{}'.format(i): (j + (i - 1) * 1) for i in range(1)}
+                for j in range(0)
+            ],
+            row_deletable=False,
+            row_selectable='single',
 
-       style={'width': '30%', 'display': 'inline-block', 'vertical-align': 'top'},
+        ),
+        style={'width': '30%', 'display': 'inline-block', 'vertical-align': 'top'},
+    ),
+    html.Div(
+        dcc.Graph(
+            id='my-graph'
+        ),
 
+        style={'width': '70%', 'display': 'inline-block', },
+        id='plot-area'
+    ),
 
-   ),
+    html.Div(
+        html.Button('Delete Row', id='Delete-rows-button', n_clicks=0)
+    ),
 
+    dcc.Dropdown(
+        options=[
+            {'label': 'Study Type', 'value': 'study_type'},
+            {'label': 'Status', 'value': 'overall_status'},
+            {'label': 'Phase', 'value': 'phase'}
+        ],
+        value='study_type',
+        id='dropdown-id',
+    ),
+    html.H1(children='Data from ' + Date),
 
-   html.Div(
-       dcc.Graph(
-           id='my-graph'
-       ),
-
-       style={'width': '70%', 'display': 'inline-block', },
-       id='plot-area'
-   ),
-   html.Div(
-       html.Button('Delete Row', id='Delete-rows-button', n_clicks=0)
-   ),
-
-   dcc.Dropdown(
-       options=[
-           {'label': 'Study Type', 'value': 'study_type'},
-           {'label': 'Status', 'value': 'overall_status'},
-           {'label': 'Phase', 'value': 'phase'}
-       ],
-       value='study_type',
-       id='dropdown-id'
-   ),
-   html.Div(id='click-data'),
 ])
 
 
 @app.callback(
-   Output('adding-rows-table', 'data'),
-   [Input('my-graph', 'clickData'),
-    Input('Delete-rows-button', 'n_clicks'),
-    Input('adding-rows-table', 'selected_rows')],
-   [State('adding-rows-table', 'data'),
-    State('adding-rows-table', 'columns')])
-def get_filter(click_data, n_clicks, selected_rows, rows, columns):
+    Output('adding-rows-table', 'data'),
+    [Input('my-graph', 'clickData'),
+     Input('Delete-rows-button', 'n_clicks')],
+    [State('adding-rows-table', 'data'),
+     State('adding-rows-table', 'columns'),
+     State('adding-rows-table', 'selected_rows'),
+     State('chart-type', 'value')])
+def on_click(click_data, n_clicks, rows, columns, selected_rows, chart_type):
     global count
-    filter = (currentGroupBy + ": " + click_data.get("points")[0].get("label"))
+    curr_filter = get_filter(chart_type, click_data)
     if n_clicks > count:
         count += 1
-        CTD.remove_filter(currentGroupBy, click_data.get("points")[0].get("label"))
+        CTD.remove_filter(currentGroupBy, curr_filter)
         return delete_filter(selected_rows, rows)
     else:
-        CTD.apply_filter(currentGroupBy, click_data.get("points")[0].get("label"))
-        return add_filter(rows, columns, filter)
+        CTD.apply_filter(currentGroupBy, curr_filter)
+        return add_filter(rows, columns, (currentGroupBy + ": " + curr_filter))
+
+
+def get_filter(chart_type, click_data):
+    if chart_type == 'pie_chart':
+        return click_data.get("points")[0].get("label")
+    elif chart_type == "bar_chart":
+        return click_data.get("points")[0].get("x")
+    else:
+        return ""
 
 
 def delete_filter(selected_rows, rows):
@@ -93,7 +113,7 @@ def delete_filter(selected_rows, rows):
     return rows
 
 
-def add_filter(rows,columns, filter):
+def add_filter(rows, columns, filter):
     is_in = check_if_exists(rows, filter)
     if not is_in:
         rows.append({c['id']: filter for c in columns})
@@ -107,8 +127,7 @@ def check_if_exists(rows, filter):
     return False
 
 
-@app.callback(Output('my-graph', 'figure'),
-             [Input('dropdown-id', 'value')])
+@app.callback(Output('my-graph', 'figure'), [Input('dropdown-id', 'value')])
 def update_plot(value):
     global currentGroupBy
     CTD.set_group_by(value)
@@ -130,4 +149,3 @@ def update_plot(value):
 
 if __name__ == '__main__':
     app.run_server(host='0.0.0.0')
-
--- tests/mocks/test_clashboard_gui.py
+++ tests/mocks/test_clashboard_gui.py
@@ -1,4 +1,4 @@
-from clashboard.clashboard_gui import add_filter,delete_filter
+from clashboard.clashboard_gui import add_filter, delete_filter, get_filter,check_if_exists
 
 columns = [{'name': 'Filters', 'id': 'column-0', 'deletable': False, 'editable_name': False}]
 
@@ -6,40 +6,70 @@ columns = [{'name': 'Filters', 'id': 'column-0', 'deletable': False, 'editable_n
 def test_add_filter_empty_list():
     rows = []
     filter = 'Observational [Patient Registry]: Study Type'
-    assert add_filter(rows,columns,filter) == [{'column-0': 'Observational [Patient Registry]: Study Type'}]
+    assert add_filter(rows, columns, filter) == [{'column-0': 'Observational [Patient Registry]: Study Type'}]
 
 
 def test_add_filter_occupied_list():
     rows = [{'column-0': 'Observational [Patient Registry]: Study Type'}]
     filter = 'Interventional: Study Type'
-    assert add_filter(rows, columns, filter) == [{'column-0': 'Observational [Patient Registry]: Study Type'},{'column-0': 'Interventional: Study Type'}]
+    assert add_filter(rows, columns, filter) == [{'column-0': 'Observational [Patient Registry]: Study Type'},
+                                                 {'column-0': 'Interventional: Study Type'}]
 
 
 def test_add_duplicate_filter():
     rows = [{'column-0': 'Observational [Patient Registry]: Study Type'}]
     filter = 'Observational [Patient Registry]: Study Type'
-    assert add_filter(rows,columns,filter) == [{'column-0': 'Observational [Patient Registry]: Study Type'}]
+    assert add_filter(rows, columns, filter) == [{'column-0': 'Observational [Patient Registry]: Study Type'}]
 
 
 def test_delete_filter_empty_list():
     rows = []
     selected_rows = [2]
-    assert delete_filter(selected_rows,rows) == []
+    assert delete_filter(selected_rows, rows) == []
 
 
 def test_delete_filter_one_in_list():
     rows = [{'column-0': 'Observational [Patient Registry]: Study Type'}]
     selected_rows = [0]
-    assert  delete_filter(selected_rows,rows) == []
+    assert delete_filter(selected_rows, rows) == []
 
 
 def test_delete_filter_two_in_list():
-    rows = [{'column-0': 'Observational [Patient Registry]: Study Type'},{'column-0': 'Interventional: Study Type'}]
+    rows = [{'column-0': 'Observational [Patient Registry]: Study Type'}, {'column-0': 'Interventional: Study Type'}]
     selected_rows = [0]
     assert delete_filter(selected_rows, rows) == [{'column-0': 'Interventional: Study Type'}]
 
 
 def test_delete_filter_middle_of_three():
-    rows = [{'column-0': 'Observational: Study Type'}, {'column-0': 'Interventional: Study Type'}, {'column-0': 'Observational [Patient Registry]: Study Type'}]
+    rows = [{'column-0': 'Observational: Study Type'}, {'column-0': 'Interventional: Study Type'},
+            {'column-0': 'Observational [Patient Registry]: Study Type'}]
     selected_rows = [1]
-    assert delete_filter(selected_rows,rows) == [{'column-0': 'Observational: Study Type'}, {'column-0': 'Observational [Patient Registry]: Study Type'}]
+    assert delete_filter(selected_rows, rows) == [{'column-0': 'Observational: Study Type'},
+                                                  {'column-0': 'Observational [Patient Registry]: Study Type'}]
+
+
+def test_get_filter_pie_chart():
+    chart_type = "pie_chart"
+    click_data = {'points': [{'curveNumber': 0, 'label': 'Observational', 'color': '#ff7f0e',
+                              'value': 56725, 'percent': 0.1877733015991764, 'v': 56725, 'i': 3, 'pointNumber': 3,
+                              'pointNumbers': [3]}]}
+    assert get_filter(chart_type, click_data) == 'Observational'
+
+
+def test_get_filter_bar_graph():
+    chart_type = "bar_chart"
+    click_data = {'points': [{'curveNumber': 0, 'pointNumber': 1, 'pointIndex': 1, 'x': 'Interventional',
+                              'y': 239276}]}
+    assert get_filter(chart_type, click_data) == 'Interventional'
+
+
+def test_if_in_is_in():
+    rows = [{'column-0': 'Observational [Patient Registry]: Study Type'},
+            {'column-0': 'Interventional: Study Type'}]
+    assert check_if_exists(rows, "Interventional: Study Type")
+
+
+def test_if_in_not_in():
+    rows = [{'column-0': 'Observational [Patient Registry]: Study Type'}]
+    assert not check_if_exists(rows, "Interventional: Study Type")
+
