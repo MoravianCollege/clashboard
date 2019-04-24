6660d252ec5d4ddd52da441c138d62faf87e7b8f
Devon <stdsh07@moravian.edu>
2019-04-24 09:38:49 -0400

    fixing formatting and adding create_curated_sponsors_column method

--- src/clashboard/sponsor_table_sifter.py
+++ src/clashboard/sponsor_table_sifter.py
@@ -15,10 +15,21 @@ class SponsorTableSifter:
         self.raw_sponsor_table = pd.read_csv(file_path)
 
     def sift_leads(self):
-        self.sifted_sponsors_table = self.raw_sponsor_table[self.raw_sponsor_table['lead_or_collaborator'].str.contains('lead')].groupby('nct_id')
-        return self.sifted_sponsors_table
+        self.sifted_sponsors_table = self.raw_sponsor_table.groupby('nct_id').filter(lambda x: len(x) > 2)\
+            .drop_duplicates(subset='nct_id')
 
     def sift_sponsors(self):
-        self.sifted_sponsors_table = self.sifted_sponsors_table[self.sifted_sponsors_table['name'].str.contains('Merck')]
-        return self.sifted_sponsors_table
+        self.sifted_sponsors_table = self.raw_sponsor_table
+        self.sifted_sponsors_table['Sifted_Sponsors'] = self.sifted_sponsors_table['name'].str.contains('Merck')
+        self.sifted_sponsors_table['Sifted_Sponsors'] = self.sifted_sponsors_table['Sifted_Sponsors']\
+            .map({True: 'Merck', False: 'Other'})
 
+    def publish_table(self):
+        file_path = '{}/100_curated_sponsors.csv'.format(self.TEST_DATA_DIR)
+        self.sifted_sponsors_table.to_csv(path_or_buf=file_path)
+
+    def create_curated_sponsors_column(self):
+        self.make_connection()
+        self.sift_leads()
+        self.sift_sponsors()
+        self.publish_table()
