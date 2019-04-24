954ace46514ae4c2e669164683a4f0c3383c1b3f
Andrew <stajc06@moravian.edu>
2019-04-24 14:53:14 -0400

    Created tests for creation of sponsor table

--- tests/test_sponsor_table_sifter.py
+++ tests/test_sponsor_table_sifter.py
@@ -1,4 +1,5 @@
 from clashboard.sponsor_table_sifter import SponsorTableSifter
+import pandas as pd
 
 
 def load_csv():
@@ -7,6 +8,20 @@ def load_csv():
     return ts
 
 
+def test_establish_base_table():
+    ts = load_csv()
+    assert not ts.raw_sponsor_table.empty
+
+
 def test_get_leads_only():
     ts = load_csv()
     ts.sift_leads()
+    assert not pd.DataFrame.equals(ts.sifted_sponsors_table, ts.raw_sponsor_table)
+
+
+def test_get_merck_only():
+    ts = load_csv()
+    ts.sift_leads()
+    comparison_table = ts.sifted_sponsors_table
+    ts.sift_sponsors()
+    assert not pd.DataFrame.equals(comparison_table, ts.sifted_sponsors_table)
