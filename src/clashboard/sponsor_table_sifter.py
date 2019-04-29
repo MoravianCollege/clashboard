0ba9401cab77000721aad4cf404854e794c7b78c
Devon <stdsh07@moravian.edu>
2019-04-28 22:32:14 -0400

    Fixed table creation error and fixed the new column algorithm.

--- src/clashboard/sponsor_table_sifter.py
+++ src/clashboard/sponsor_table_sifter.py
@@ -20,40 +20,45 @@ class SponsorTableSifter:
         self.password = os.getenv('password')
 
     def make_connection(self):
-        sql_command = 'SELECT * from ctgov.sponsors'
+        print('Establishing connection to local database...\n')
         self.conn = psycopg2.connect(host=self.hostname,
                                      database=self.database,
                                      user=self.username,
                                      password=self.password)
+
+    def query_database(self):
+        print('Collecting Data from local Database...\n')
+        sql_command = 'SELECT * from ctgov.sponsors'
         self.raw_sponsor_table = pd.read_sql(sql=sql_command, con=self.conn)
 
     def sift_leads(self):
+        print('Removing duplicate sponsors...\n')
         self.sifted_sponsors_table = self.raw_sponsor_table.groupby('nct_id').filter(lambda x: len(x) > 2)\
             .drop_duplicates(subset='nct_id')
 
-    def sift_sponsors(self):
-        self.sifted_sponsors_table = self.raw_sponsor_table
-        self.sifted_sponsors_table['Sifted_Sponsors'] = self.sifted_sponsors_table['name'].str.contains('Merck')
-        self.sifted_sponsors_table['Sifted_Sponsors'] = self.sifted_sponsors_table['Sifted_Sponsors']\
-            .map({True: 'Merck', False: 'Other'})
-
     def curate_all_sponsors(self):
-        sponsor_names = {'Merck', 'Phiser', 'Aventis', 'NCI'}
+        print('Checking names against known sponsors, and creating new column.\n')
+        sponsor_names = {'Merck', 'Pfizer', 'NCI'}
 
         self.sifted_sponsors_table = self.raw_sponsor_table
+        self.sifted_sponsors_table['curated_sponsors'] = pd.Series()
 
-        for name in sponsor_names:
-            self.sifted_sponsors_table['Sifted_Sponsors'] = self.sifted_sponsors_table['name']\
-                .str.contains(name)
-
-            self.sifted_sponsors_table['Sifted_Sponsors'] = self.sifted_sponsors_table['Sifted_Sponsors'] \
-                .map({True: name})
+        for index in self.sifted_sponsors_table.index:
+            for name in sponsor_names:
+                if name in self.sifted_sponsors_table['name'][index]:
+                    self.sifted_sponsors_table['curated_sponsors'][index] = name
+                else:
+                    self.sifted_sponsors_table['curated_sponsors'][index] = 'Other'
 
     def publish_table(self):
-        self.sifted_sponsors_table.to_sql(name='curated_sponsors', con=self.conn)
+        print('Publishing new table to local database.\n')
+        self.sifted_sponsors_table=self.sifted_sponsors_table[['nct_id', 'curated_sponsors']]
+        self.sifted_sponsors_table.to_sql(name='curated_sponsors_table', con=self.conn)
 
     def create_curated_sponsors_column(self):
         self.make_connection()
+        self.query_database()
         self.sift_leads()
         self.curate_all_sponsors()
         self.publish_table()
+        print('Done.')
