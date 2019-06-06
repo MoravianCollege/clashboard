5849a9eb5554063ee584f74883978e0a1decff58
Jonah Beers <beersj02@moravian.edu>
2019-06-06 16:41:19 -0400

    Add line to run get_most_recent_data script

--- scripts/instance_setup.sh
+++ scripts/instance_setup.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash expect
 
 apt update
 apt install -y python3-pip
@@ -8,5 +8,8 @@ cd clashboard
 pip3 install -r requirements.txt
 pip3 install -e .
 echo "hostname=localhost\nport=5432\ndatabase=aact\nusername=postgres\npassword=your_password" > .env
-sudo -u postgres psql template1 -c "ALTER USER postgres with password 'your_password';"
+-u postgres psql template1 -c "ALTER USER postgres with password 'your_password';"
 sed -i 's/local   all             postgres                                peer/local   all             postgres                                md5/g' /etc/postgresql/10/main/pg_hba.conf
+systemctl restart postgresql.service
+~/scripts/get_most_recent_data.sh
+send "your_password"
