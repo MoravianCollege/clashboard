84bd57d23a283aa8b852d92677f3a6a7dfea89d9
Jonah Beers <beersj02@moravian.edu>
2019-06-06 15:46:23 -0400

    Add lines related to postgres password

--- scripts/instance_setup.sh
+++ scripts/instance_setup.sh
@@ -8,3 +8,6 @@ cd clashboard
 pip3 install -r requirements.txt
 pip3 install -e .
 echo "hostname=localhost\nport=5432\ndatabase=aact\nusername=postgres\npassword=your_password" > .env
+sudo -u postgres psql template1
+ALTER USER postgres with password 'your_password';
+\q
