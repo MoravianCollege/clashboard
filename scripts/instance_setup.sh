6d1b92ba7a2776d606fb7f0050b6341f1b2b0f55
Jonah Beers <beersj02@moravian.edu>
2019-06-06 15:36:29 -0400

    Remove -e from echo that directs to .env

--- scripts/instance_setup.sh
+++ scripts/instance_setup.sh
@@ -7,4 +7,4 @@ apt install -y unzip
 cd clashboard
 pip3 install -r requirements.text
 pip3 install -e .
-echo -e "hostname=localhost\nport=5432\ndatabase=aact\nusername=postgres\npassword=your_password" > .env
+echo $"hostname=localhost\nport=5432\ndatabase=aact\nusername=postgres\npassword=your_password" > .env
