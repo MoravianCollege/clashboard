8993d02f049fd10bbcb1d0c8eeddee2db1cee2fd
Jonah Beers <beersj02@moravian.edu>
2019-06-06 16:43:39 -0400

    Add sudo to two lines

--- scripts/instance_setup.sh
+++ scripts/instance_setup.sh
@@ -1,4 +1,4 @@
-#!/bin/bash expect
+#!/bin/bash
 
 apt update
 apt install -y python3-pip
@@ -8,8 +8,7 @@ cd clashboard
 pip3 install -r requirements.txt
 pip3 install -e .
 echo "hostname=localhost\nport=5432\ndatabase=aact\nusername=postgres\npassword=your_password" > .env
--u postgres psql template1 -c "ALTER USER postgres with password 'your_password';"
+sudo -u postgres psql template1 -c "ALTER USER postgres with password 'your_password';"
 sed -i 's/local   all             postgres                                peer/local   all             postgres                                md5/g' /etc/postgresql/10/main/pg_hba.conf
 systemctl restart postgresql.service
-~/scripts/get_most_recent_data.sh
-send "your_password"
+sudo ~/scripts/get_most_recent_data.sh
