9275f7d71667865c5964a9d8af73e16fc1603a41
Jonah Beers <beersj02@moravian.edu>
2019-06-06 15:25:06 -0400

    Add install requirements and .env creation

--- scripts/instance_setup.sh
+++ scripts/instance_setup.sh
@@ -4,3 +4,7 @@ sudo apt update
 sudo apt install -y python3-pip
 sudo apt install -y postgresql
 sudo apt install -y unzip
+cd clashboard
+sudo pip3 install -r requirements.text
+sudo pip3 install -e .
+sudo echo -e "hostname=localhost\nport=5432\ndatabase=aact\nusername=postgres\npassword=your_password" > .env
