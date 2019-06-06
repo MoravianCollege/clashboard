465d534af68a40dc58d3e55f46cc190a1b79c9d3
Jonah Beers <beersj02@moravian.edu>
2019-06-06 16:02:24 -0400

    Change psql command to automatically execute and exit

--- scripts/instance_setup.sh
+++ scripts/instance_setup.sh
@@ -8,6 +8,4 @@ cd clashboard
 pip3 install -r requirements.txt
 pip3 install -e .
 echo "hostname=localhost\nport=5432\ndatabase=aact\nusername=postgres\npassword=your_password" > .env
-sudo -u postgres psql template1
-ALTER USER postgres with password 'your_password';
-\q
+sudo -u postgres psql template1 -c "ALTER USER postgres with password 'your_password';"
