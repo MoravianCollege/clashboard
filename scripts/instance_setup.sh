29506c656c3b952b6697610732d8900fcdc3a910
Jonah Beers <beersj02@moravian.edu>
2019-06-06 15:39:25 -0400

    Fixed requirements file typo

--- scripts/instance_setup.sh
+++ scripts/instance_setup.sh
@@ -5,6 +5,6 @@ apt install -y python3-pip
 apt install -y postgresql
 apt install -y unzip
 cd clashboard
-pip3 install -r requirements.text
+pip3 install -r requirements.txt
 pip3 install -e .
-echo $"hostname=localhost\nport=5432\ndatabase=aact\nusername=postgres\npassword=your_password" > .env
+echo "hostname=localhost\nport=5432\ndatabase=aact\nusername=postgres\npassword=your_password" > .env
