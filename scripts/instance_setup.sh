2401f8a1d71cf987380507192b7e78d31788b8f7
Jonah Beers <beersj02@moravian.edu>
2019-06-07 11:30:25 -0400

    Create environment variable for password

--- scripts/instance_setup.sh
+++ scripts/instance_setup.sh
@@ -8,8 +8,8 @@ apt install -y unzip
 cd clashboard
 pip3 install -r requirements.txt
 pip3 install -e .
-echo "hostname=localhost\nport=5432\ndatabase=aact\nusername=postgres\npassword=your_password" > .env
-sudo -u postgres psql template1 -c "ALTER USER postgres with password 'your_password';"
+echo "hostname=localhost\nport=5432\ndatabase=aact\nusername=postgres\npassword=$PSQL_PASSWORD" > .env
+sudo -u postgres psql template1 -c "ALTER USER postgres with password '$PSQL_PASSWORD';"
 sed -i 's/local   all             postgres                                peer/local   all             postgres                                md5/g' /etc/postgresql/10/main/pg_hba.conf
 systemctl restart postgresql.service
 # manually enter password?
