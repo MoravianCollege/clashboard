a9355bbf58767301a4943d52550d4c96ab993948
Jonah Beers <beersj02@moravian.edu>
2019-06-10 08:51:38 -0400

    Test user input for new password

--- scripts/instance_setup.sh
+++ scripts/instance_setup.sh
@@ -8,8 +8,10 @@ apt install -y unzip
 cd clashboard
 pip3 install -r requirements.txt
 pip3 install -e .
-echo "hostname=localhost\nport=5432\ndatabase=aact\nusername=postgres\npassword=$PSQL_PASSWORD" > .env
-sudo -u postgres psql template1 -c "ALTER USER postgres with password '$PSQL_PASSWORD';"
+echo "Enter new password for postgres on PSQL:"
+set /p PSQLpassword=Enter new postgres password for PSQL:
+echo "hostname=localhost\nport=5432\ndatabase=aact\nusername=postgres\npassword=%PSQLpassword%" > .env
+sudo -u postgres psql template1 -c "ALTER USER postgres with password '%PSQLpassword%';"
 sed -i 's/local   all             postgres                                peer/local   all             postgres                                md5/g' /etc/postgresql/10/main/pg_hba.conf
 systemctl restart postgresql.service
 # manually enter password?
